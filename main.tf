3c37315cd05b4afa1f49352f15a1cfe32516c763
cbrewood <cb5869@att.com>
2021-02-02 15:15:29 +0000

    New AMI branch1

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ provider "random" {}
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
+  ami                    = "ami-06bf54a3407e2fed4"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
