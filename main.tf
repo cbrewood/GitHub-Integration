b83889d4d80ca54560f4e68ad487da84d1007851
cbrewood <cb5869@att.com>
2021-02-03 16:51:13 +0000

    change region bast to west changed AMI

--- main.tf
+++ main.tf
@@ -18,10 +18,10 @@ terraform {
 }
 provider "aws" {
   profile = "default"
-  region  = "us-east-2"
+  region  = "us-west-2"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
 }
