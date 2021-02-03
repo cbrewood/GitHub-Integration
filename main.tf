35803dd84aba99aa665f95abe771e39b08998d91
cbrewood <cb5869@att.com>
2021-02-03 17:37:57 +0000

    Add ec2 to us-west-2

--- main.tf
+++ main.tf
@@ -1,19 +1,18 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-  }
-
-  backend "remote" {
-    organization = "AWS_Automated"
-
-    workspaces {
-      name = "GitHub-Integration"
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
     }
   }
 }
 
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+}
