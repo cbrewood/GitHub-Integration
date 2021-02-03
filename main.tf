2be4268c201a3edacdf8003613fa4f04ebf59923
cbrewood <cb5869@att.com>
2021-02-03 17:27:31 +0000

    detroy 1

--- main.tf
+++ main.tf
@@ -1,1 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+
+  backend "remote" {
+    organization = "AWS_Automated"
+
+    workspaces {
+      name = "GitHub-Integration"
+    }
+  }
+}
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+}
 terraform destroy
