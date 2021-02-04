bd5adc2fad413ae4b437f6dc6ca027ac1ef2fea6
cbrewood <cb5869@att.com>
2021-02-04 12:17:57 +0000

    update main file to add eu-west-1 instance aws instance example2

--- main.tf
+++ main.tf
@@ -16,3 +16,12 @@ resource "aws_instance" "example" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 }
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-1"
+}
+
+resource "aws_instance" "example2" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+}
