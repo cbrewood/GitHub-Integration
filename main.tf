d035de7d2169e9e79eb5e09fcf24b2354cbfeb44
cbrewood <cb5869@att.com>
2021-02-04 12:40:20 +0000

    updated main file removing dual provider reference

--- main.tf
+++ main.tf
@@ -16,10 +16,10 @@ resource "aws_instance" "example" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 }
-provider "aws" {
+
+
   profile = "default"
-  region  = "eu-west-1"
-}
+  region  = "us-west-1"
 
 resource "aws_instance" "example2" {
   ami           = "ami-830c94e3"
