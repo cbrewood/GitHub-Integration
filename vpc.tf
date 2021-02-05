6fe5fa4576857b66f7d91083c5a58ac12d6ad3dc
cbrewood <cb5869@att.com>
2021-02-05 17:16:52 +0000

    12

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,4 @@
-provider "aws" {
+ terraform {
   region = "us-west-1"
 }
 
@@ -12,3 +12,4 @@ module "vpc" {
   azs             = ["eu-west-1a", "eu-west-1b", "euw1-az3"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  }
