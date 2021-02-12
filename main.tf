eef7653241616879ba8e3635cce6d2724a6d89f3
cbrewood <cb5869@att.com>
2021-02-12 10:21:33 +0000

    3

--- main.tf
+++ main.tf
@@ -7,22 +7,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  profile = "default"
-  region  = "us-west-2"
-}
 
-resource "aws_instance" "example" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-}
 
-provider "aws" {
-  alias  = "east"
-  region = "us-east-1"
-}
 
-resource "aws_instance" "us-east-1" {
-  ami           = "ami-b374d5a5"
-  instance_type = "t2.micro"
-}
