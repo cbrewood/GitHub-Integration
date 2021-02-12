a9f671f798c64dfff629050abc7b272bdd5b723b
cbrewood <cb5869@att.com>
2021-02-12 11:50:54 +0000

    20

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ terraform {
       version = "~> 3.10.0"
     }
   }
-
+  {
       east = {
       source  = "hashicorp/aws"
       version = "~> 3.10.0"
     }
 }
-
+}
 
 provider "aws" {
   profile = "default"
@@ -24,6 +24,7 @@ resource "aws_instance" "example" {
 }
 
 provider "aws" {
+  alias  = "east"
   region = "us-east-1"
 }
 
@@ -31,4 +32,5 @@ resource "east_instance" "us-east-1" {
   ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
 }
+
 
